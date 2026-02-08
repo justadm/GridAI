b74bfc4b28552a3f8e8ae5c1b6420b836d7a33e5
just <adm@biwt.ru>
2026-02-09 01:11:18 +0300

    Allow sbdb.loc host in Vite

--- apps/web-ssr/vite.config.ssr.ts
+++ apps/web-ssr/vite.config.ssr.ts
@@ -5,6 +5,9 @@ import path from 'path';
 export default defineConfig({
   root: path.resolve(__dirname),
   plugins: [vue()],
+  server: {
+    allowedHosts: ['sbdb.loc', 'localhost', '127.0.0.1']
+  },
   build: {
     outDir: path.resolve(__dirname, '../../dist/server'),
     ssr: '/src/entry-server.ts'
